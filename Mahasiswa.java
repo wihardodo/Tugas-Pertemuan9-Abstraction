7e44a60000922e7c7ec90714f17f0d92cc7c9582
wihardodo <wihardodo@gmail.com>
2017-05-09 23:04:53 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,16 @@
+package oop.abstraction;
+
+public abstract class Mahasiswa {
+
+    protected String nama = "Wihardodo";
+    protected String nim = "10112547";
+
+    protected abstract void Kuliah(); 
+	protected abstract void Lulus(); 
+	protected abstract void Tidaklulus(); 
+        return nama;
+    }
+
+    public abstract void printNama();
+
+}
---
+++ MainProgram.java
@@ -0,0 +1,20 @@
+package oop.abstraction;
+
+public class MainProgram {
+
+	public static void main (String[] args) {
+		Mahasiswa Wihardodo = new Mahasiswa ()}
+			@Override
+			protected void Kuliah() {
+				throw new ("Rajin belajar dan disiplin");
+    }
+			@Override
+			protected void Lulus() {
+				throw new ("Harus Lulus 4 Tahun");
+    }
+			@Override
+			protected void TidakLulus() {
+				throw new ("Wihardodo tidak boleh tidak Lulus");
+    }
+
+}
